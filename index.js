e161cffd0ccc80535eae41670d574aaf16a8952f
afzal mahmud <afzzalmahmud@gmail.com>
2022-01-19 06:45:31 -0800

    implement delete product on admin

--- index.js
+++ index.js
@@ -175,6 +175,14 @@ async function run() {
           res.json(result)
         })
 
+        /* delete a single item based on id */ 
+        app.delete('/deleteproduct/:id',async(req,res) =>{
+          const id = req.params.id;
+          const query = {_id : ObjectId(id)}
+          const result = await allGuitarCollection.deleteOne(query)
+          res.json(result)
+        })
+
     }
     finally{
         // await client.close()
